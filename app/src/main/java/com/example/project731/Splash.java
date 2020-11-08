01b68f2bb5a37fcf76f73aaf38745cb1363c6257
carlsondultra <carlsondultra@users.noreply.github.com>
2020-11-07 23:22:06 -0500

    Added music playing during splash screen

--- app/src/main/java/com/example/project731/Splash.java
+++ app/src/main/java/com/example/project731/Splash.java
@@ -3,6 +3,7 @@ package com.example.project731;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import java.util.Timer;
@@ -10,14 +11,22 @@ import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
+    MediaPlayer mdSong;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mdSong = new MediaPlayer();
+        mdSong = MediaPlayer.create(this,R.raw.love);
+        mdSong.start();
+
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 finish();
+                mdSong.stop();
                 startActivity(new Intent(Splash.this,MapsActivity.class));
             }
         };
