35828c7d3a4c155a343d7807f33f3760b43dc23c
carlsondultra <carlsondultra@users.noreply.github.com>
2020-11-18 19:51:06 -0500

    Added orientation sensor capabilities

--- app/src/main/java/com/example/project731/FirebaseMenuActivity.java
+++ app/src/main/java/com/example/project731/FirebaseMenuActivity.java
@@ -3,18 +3,37 @@ package com.example.project731;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class FirebaseMenuActivity extends AppCompatActivity {
 
+
+
     private Button back;
     private Button btnAbout;
     private Button stores;
+    private ImageView imageView;
+
+    private SensorManager sensorManager;
+    private Sensor sensorAccelerometer;
+    private Sensor sensorMagneticField;
+
+    private float[] floatGravity = new float[3];
+    private float[] floatGeoMagnetic = new float[3];
+
+    private float[] floatOrientation = new float[3];
+    private float[] floatRotationMatrix = new float[9];
+
 
 
     @Override
@@ -25,6 +44,54 @@ public class FirebaseMenuActivity extends AppCompatActivity {
         back = findViewById(R.id.back);
         btnAbout = findViewById(R.id.btnAbout);
         stores = findViewById(R.id.stores);
+        imageView = findViewById(R.id.imageView);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorMagneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        SensorEventListener sensorEventListenerAccelerometer = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                floatGravity = sensorEvent.values;
+
+                SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity, floatGeoMagnetic);
+                SensorManager.getOrientation(floatRotationMatrix, floatOrientation);
+
+                imageView.setRotation((float) (floatOrientation[0]*180/3.14159)); //negative sign may be positive instead depending on the phone
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        SensorEventListener sensorEventListenerMagneticField = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                floatGeoMagnetic = sensorEvent.values;
+
+                SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity, floatGeoMagnetic);
+                SensorManager.getOrientation(floatRotationMatrix, floatOrientation);
+
+                imageView.setRotation((float) (floatOrientation[0]*180/3.14159)); //negative sign may be positive instead depending on the phone
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+
+        sensorManager.registerListener(sensorEventListenerAccelerometer, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListenerMagneticField, sensorMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
+
+
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,7 +139,5 @@ public class FirebaseMenuActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-
     }
 }
