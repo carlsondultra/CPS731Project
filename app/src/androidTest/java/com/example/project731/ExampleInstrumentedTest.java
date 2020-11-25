e202a40b025b075f26c92306cf778b38ac2e0a24
carlsondultra <carlsondultra@users.noreply.github.com>
2020-11-25 18:31:15 -0500

    adding first instrumented test, checking if activity pops up after splash screen

--- app/src/androidTest/java/com/example/project731/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/project731/ExampleInstrumentedTest.java
@@ -1,13 +1,20 @@
 package com.example.project731;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.SystemClock;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +24,25 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule mActivityRule = new ActivityScenarioRule<>(Splash.class);
+
+    //Test checking if the Splash Screen goes to the main screen
+    @Test
+    public void activityLaunch(){
+        SystemClock.sleep(5200);
+        onView(withId(R.id.register)).perform(click());
+    }
+
+
+    /*
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.project731", appContext.getPackageName());
     }
+
+     */
 }
