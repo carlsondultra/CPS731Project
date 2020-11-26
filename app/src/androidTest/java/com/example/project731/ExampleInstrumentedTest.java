4048c5d33cc8d8403c612209743d6ea6944b86a2
carlsondultra <carlsondultra@users.noreply.github.com>
2020-11-25 19:24:28 -0500

    Added register and login tests

--- app/src/androidTest/java/com/example/project731/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/project731/ExampleInstrumentedTest.java
@@ -12,8 +12,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
@@ -28,6 +30,12 @@ public class ExampleInstrumentedTest {
     @Rule
     public ActivityScenarioRule mActivityRule = new ActivityScenarioRule<>(Splash.class);
 
+    private String maleUsername = "instrumentedmale@m.com";
+    private String malePassword = "123456";
+
+    private String loginUsername = "male@m.com";
+    private String loginPassword = "123456";
+
     //Test checking if the Splash Screen goes to the main screen
     @Test
     public void activityLaunch(){
@@ -38,13 +46,65 @@ public class ExampleInstrumentedTest {
     //Test checking if store locator works
     @Test
     public void storeActivityLaunch(){
-        SystemClock.sleep(5200);
+        SystemClock.sleep(6000);
         onView(withId(R.id.menu)).perform(click());
+        SystemClock.sleep(1000);
         onView(withId(R.id.stores)).perform(click());
         SystemClock.sleep(1000);
         onView(withId(R.id.back)).perform(click());
     }
 
+    //Test checking if the menu works
+    @Test
+    public void menuActivityLaunch(){
+        SystemClock.sleep(6000);
+        onView(withId(R.id.menu)).perform(click());
+        SystemClock.sleep(1000);
+        onView(withId(R.id.back)).perform(click());
+    }
+
+    //Test checking if the about us section works
+    @Test
+    public void aboutActivityLaunch(){
+        SystemClock.sleep(6000);
+        onView(withId(R.id.menu)).perform(click());
+        SystemClock.sleep(1000);
+        onView(withId(R.id.btnAbout)).perform(click());
+    }
+
+    //Test checking if registration works
+    //maleUsername needs to be changed every time this test is run because there cannot be two of the same usernames.
+    @Test
+    public void registerActivityLaunch(){
+        SystemClock.sleep(6000);
+        onView(withId(R.id.register)).perform(click());
+        SystemClock.sleep(1000);
+        onView(withId(R.id.email_login)).perform(typeText(maleUsername)); //inputs text into text box
+        closeSoftKeyboard(); // close the keyboard
+        onView(withId(R.id.name)).perform(typeText("name"));
+        closeSoftKeyboard();
+        onView(withId(R.id.password_first)).perform(typeText(malePassword));
+        closeSoftKeyboard();
+        onView(withId(R.id.password_confirm_box)).perform(typeText(malePassword));
+        closeSoftKeyboard();
+        onView(withId(R.id.male)).perform(click());
+        onView(withId(R.id.register)).perform(click());
+        SystemClock.sleep(2000);
+    }
+
+    //Test checking if login works
+    @Test
+    public void loginActivityLaunch(){
+        SystemClock.sleep(6000);
+        onView(withId(R.id.login)).perform(click());
+        SystemClock.sleep(1000);
+        onView(withId(R.id.email_login)).perform(typeText(loginUsername));
+        closeSoftKeyboard();
+        onView(withId(R.id.password_confirm_box)).perform(typeText(loginPassword));
+        closeSoftKeyboard();
+        onView(withId(R.id.login)).perform(click());
+        SystemClock.sleep(2000);
+    }
 
     /*
     @Test
